d632cfe31e63e92a9edabc1661c7917a4ce5ae7c
CarolinaAR10 <caritogold17@gmail.com>
2025-12-24 11:12:30 -0600

    Agregando la vista de dashboard del empleado
    Add: Agregando la vista de dashboard del empleado

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -2,6 +2,26 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'employee/dashboard',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'chef/dashboard',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'chef/statistics',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'chef/menu-management',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'chef/order-management',
+    renderMode: RenderMode.Prerender
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,49 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/employee/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'employee',
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./modules/employee/employee-dashboard/employee-dashboard.component').then(m => m.EmployeeDashboardComponent)
+      }
+    ]
+  },
+  {
+    path: 'chef',
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./modules/chef/chef-dashboard/chef-dashboard.component').then(m => m.ChefDashboardComponent)
+      },
+      {
+        path: 'statistics',
+        loadComponent: () => import('./modules/chef/chef-statistics/chef-statistics.component').then(m => m.ChefStatisticsComponent)
+      },
+      {
+        path: 'menu-management',
+        loadComponent: () => import('./modules/chef/menu-management/menu-management.component').then(m => m.MenuManagementComponent)
+      },
+      {
+        path: 'order-management',
+        loadComponent: () => import('./modules/chef/order-management/order-management.component').then(m => m.OrderManagementComponent)
+      }
+    ]
+  }
+];

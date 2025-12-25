9fec40662f16cbf2b4afb5bc60f1a17044639d12
Yureli201 <yurelimartinezmartinez@gmail.com>
2025-12-25 12:45:15 -0600

    Layout del login y conexion con API

--- frontend/src/app/modules/auth/components/login/login.component.ts
+++ frontend/src/app/modules/auth/components/login/login.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../../../core/services/auth.service';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  credentials = {
+    username: '',
+    password: ''
+  };
 
+  constructor(private auth: AuthService) {}
+
+  onSubmit() {
+    console.log('Attempting login with:', this.credentials);
+    this.auth.login(this.credentials).subscribe({
+      next: (response:any) => {
+        console.log('Login successful', response);
+        sessionStorage.setItem('token', response.access_token);
+        sessionStorage.setItem('expiredToken', response.refresh_token)
+      },
+      error: (error) => {
+        console.error('Login failed', error);
+      }
+    });
+  }
 }

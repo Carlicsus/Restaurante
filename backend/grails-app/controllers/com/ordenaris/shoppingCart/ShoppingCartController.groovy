ad87e5230ca89eebcc1e25cdbb93a1541a9a96af
Raul Reyes <raulreyesbatalla@outlook.es>
2026-01-01 11:42:37 -0600

    Arreglando shopping cart

--- backend/grails-app/controllers/com/ordenaris/shoppingCart/ShoppingCartController.groovy
+++ backend/grails-app/controllers/com/ordenaris/shoppingCart/ShoppingCartController.groovy
@@ -14,16 +14,7 @@ class ShoppingCartController {
         return respond(serviceResponse.resp, status: serviceResponse.status)
     }
     def listOrderShoppingCartByUser(){
-        def data = [
-            id: params.id,
-            max: params.max,
-            offset: params.offset,
-            sort: params.sort,
-            order: params.order,
-            status: params.status,
-            query: params.query
-        ]
-        def serviceResponse = shoppingCartService.listOrderShoppingCartByUser(data, auth)
+        def serviceResponse = shoppingCartService.listOrderShoppingCartByUser(auth) 
         return respond(serviceResponse.resp, status: serviceResponse.status)
     }
     def newOrderShoppingCart(){

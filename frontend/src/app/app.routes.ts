581c5a7c5e32e7591c8195f75dc89fb34c9533b5
CarolinaAR10 <caritogold17@gmail.com>
2026-01-01 17:28:42 -0600

    Se elimina la vista de cobranza y se modifican los navbar
    Delete: quitando la vista de cobranza y arreglando el estilo de los navbar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -23,7 +23,6 @@ import { CloneSaucerComponent } from './modules/chef/clone-saucer/clone-saucer.c
 //finance
 import { FinanceDashboardComponent } from './modules/finance/finance-dashboard/finance-dashboard.component';
 import { DebtManagementComponent } from './modules/finance/debt-management/debt-management.component';
-import { PaymentManagementComponent } from './modules/finance/payment-management/payment-management.component';
 import { PaymentEmployeeComponent } from './modules/finance/payment-employee/payment-employee.component';
 
 import { LandingComponent } from './modules/home/pages/landing/landing.component';
@@ -43,7 +42,6 @@ export const routes: Routes = [
   { path:'employee/rate-dish', component: RateDishComponent},
   { path:'finance/dashboard', component: FinanceDashboardComponent },
   { path:'finance/debt', component: DebtManagementComponent },
-  { path:'finance/payment', component: PaymentManagementComponent },
   { path:'finance/payment-employee', component: PaymentEmployeeComponent },
   { path:'chef/dashboard', component: ChefDashboardComponent },
   { path:'chef/menu-management', component: MenuManagementComponent },

b71952b7c00c745590553e16c3637b19acc49d12
Raul Reyes <raulreyesbatalla@outlook.es>
2026-01-01 20:20:24 -0600

    Quitando comentarios

--- backend/grails-app/services/com/ordenaris/schedule/ScheduleService.groovy
+++ backend/grails-app/services/com/ordenaris/schedule/ScheduleService.groovy
@@ -46,8 +46,6 @@ class ScheduleService {
         
         LocalTime nowLocal = LocalTime.now(java.time.ZoneId.of("America/Mexico_City"))
         Time nowSql = Time.valueOf(nowLocal)
-        println "1. Hora detectada por Java (LocalTime): ${nowLocal}"
-        println "2. Hora convertida para SQL (Time):     ${nowSql}"
         def count = Schedule.createCriteria().count {
             eq("isWorking", true)
             le("entryTime", nowSql) 

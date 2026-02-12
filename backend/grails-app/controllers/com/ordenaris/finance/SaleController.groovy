782b403c40d3b2b13ab3380f3bb55d4827f4b8c7
Zac-ek <a.zacek.gc@gmail.com>
2026-02-12 11:55:28 -0600

    Prueba

--- backend/grails-app/controllers/com/ordenaris/finance/SaleController.groovy
+++ backend/grails-app/controllers/com/ordenaris/finance/SaleController.groovy
@@ -13,6 +13,7 @@ class SaleController {
 
     @Secured(['ROLE_FINANCE','ROLE_ADMIN'])
     def listDebtors() {
+        println "HOLA"
         def response = saleService.listDebtors()
         return respond(response.resp, status: response.status)
     }

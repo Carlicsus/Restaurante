ab3563a952bdee5506750d7c7390eef872bd2777
Zac-ek <a.zacek.gc@gmail.com>
2026-02-12 18:21:21 -0600

    Cambio controlador de ordenes

--- backend/grails-app/controllers/com/ordenaris/shoppingCart/ShoppingCartController.groovy
+++ backend/grails-app/controllers/com/ordenaris/shoppingCart/ShoppingCartController.groovy
@@ -8,7 +8,7 @@ class ShoppingCartController {
 	static responseFormats = ['json']
     def shoppingCartService
     SpringSecurityService springSecurityService
-    private getAuth() { springSecurityService.principal }
+    private getAuth() { springSecurityService.currentUser }
     def listOrderShoppingCart(){
         def serviceResponse = shoppingCartService.listOrderShoppingCart() 
         return respond(serviceResponse.resp, status: serviceResponse.status)
@@ -20,7 +20,7 @@ class ShoppingCartController {
     def newOrderShoppingCart(){
         def data = request.JSON
         for (item in data){
-                if (!item.dishId) {
+                if (!item.dishUuid) {
                     return respond([success: false, message: "Falta el ID del platillo"], status: 400)
                 }
                 if (!item.quantityDish || item.quantityDish <= 0) {
@@ -50,9 +50,6 @@ class ShoppingCartController {
         def dataP = params
         for (item in dataR){
             if(!dataR){
-                if (!dataR.user_id) {
-                    return respond([success: false, message: "Falta el ID del usuario"], status: 400)
-                }
                 if (!dataR.dishUuid) {
                     return respond([success: false, message: "Falta el ID del platillo"], status: 400)
                 }
